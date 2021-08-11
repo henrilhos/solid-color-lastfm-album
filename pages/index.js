0c7afa13d1e8a148fb3fe2200dabc56b75d3a374
Henrique de Castilhos <henrique@castilhos.dev>
2021-08-10 23:01:01 -0300

    Fix API params

--- pages/index.js
+++ pages/index.js
@@ -37,7 +37,7 @@ export default function Home() {
       setAlbumURL(formik.values.albumURL);
       axios
         .get("/api/get-cover-color", {
-          params: { albumURL },
+          params: { albumURL: formik.values.albumURL },
         })
         .then(({ data }) => {
           setColor(data.background);
